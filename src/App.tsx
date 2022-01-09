1a477ca8da93ce1af8dfbf1157091b4ba80c6c07
jinwook-song <wlsdnr129@naver.com>
2022-01-09 13:17:20 +0900

    styled components with type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+
+interface IBox {
+  bgColor?: string;
+}
+
+const Box = styled.div<IBox>`
+  background-color: ${(props) => props.bgColor ?? 'teal'};
+  width: 100px;
+  height: 100px;
+`;
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Box />
+      <Box bgColor='tomato' />
+    </Container>
+  );
 }
 
 export default App;
